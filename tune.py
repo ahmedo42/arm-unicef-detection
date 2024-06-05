93356dc1ddffe31c81d2c80336b1155bd0731e10
Ahmed Omar <ahmed42795@gmail.com>
2024-06-05 11:49:11 +0300

    remove wandb logging from tuning

--- tune.py
+++ tune.py
@@ -4,7 +4,7 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("--seed", type=int, default=42)
 parser.add_argument("--iterations", type=int, default=300)
-parser.add_argument("--epochs", type=int, default=50)
+parser.add_argument("--epochs", type=int, default=30)
 parser.add_argument("--batch", type=int, default=64)
 parser.add_argument("--dataset_path", type=str, default='./data')
 parser.add_argument("--model_name", type=str, default='yolov8n.pt')
@@ -15,12 +15,11 @@ args = parser.parse_args()
 def tune():
     config = vars(args)
     train_df = load_data(config['dataset_path'])
-    setup_logging(job_type='tuning')
     model = YOLO(config['model_name'])
     images_src =  config['dataset_path'] +"/Images/"
     images_train_target = f"./experiment/tune/images"
-    copy_images(train_df['image_id'].unique(),images_src,images_train_target)
     yamls = setup_experiment(train_df,exp_type='fit',seed=config['seed'])
+    copy_images(train_df['image_id'].unique(),images_src,images_train_target)
 
     model.tune(
         data = yamls[0],
